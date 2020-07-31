41a9ace456cde8b465bcd12900411e620c447c49
VladimirKhuraskin <khuraskin.dev@gmail.com>
2020-07-31 07:02:07 +0400

    Initial commit

---
+++ AppboosterSDKExample/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  AppboosterSDKExample
+//
+//  Created by Appbooster on 31.07.2020.
+//  Copyright © 2020 Appbooster. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+  }
+
+}
